0f6157c45a78d69d08b3e78a11e5ba27f44743f0
Shane <shane.koester.contact@gmail.com>
2024-03-02 04:53:05 -0400

    cleaned code

--- src/components/phone/Screen.jsx
+++ src/components/phone/Screen.jsx
@@ -1,153 +1,136 @@
 import React, { useState, useEffect, useRef } from 'react';
 import {
-    ScreenContainer,
-    ScreenHeader,
-    HeaderProfile,
-    ProfileImage,
-    ProfileName,
-    HeaderBackBtn,
-    HeaderCallBtn,
-    InputContainer,
-    InputTextArea,
-    Messages,
-    SentMessage,
-    ReceivedMessage,
-    HeaderInfo,
+   ScreenContainer,
+   ScreenHeader,
+   HeaderProfile,
+   ProfileImage,
+   ProfileName,
+   HeaderBackBtn,
+   HeaderCallBtn,
+   InputContainer,
+   InputTextArea,
+   Messages,
+   SentMessage,
+   ReceivedMessage,
+   HeaderInfo,
+   ChatButton,
 } from './ScreenStyles';
 import OpenAI from 'openai';
 
+const openai = new OpenAI({
+   apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+   dangerouslyAllowBrowser: true,
+});
+
+// create thread
+const thread = await openai.beta.threads.create();
+
 const Screen = () => {
-    const [inputValue, setInputValue] = useState('');
-    const [messageList, setMessageList] = useState([
-        { text: "Hey, I'm Shane.", sender: 'shane' },
-        { text: 'How can I help you?', sender: 'shane' },
-    ]);
-    const messagesEndRef = useRef(null);
+   const [textAreaValue, setTextAreaValue] = useState('');
+   const [messageList, setMessageList] = useState([
+      { text: "Hey, I'm Shane's Assistant.", sender: 'assistant' },
+      { text: 'How can I help you?', sender: 'assistant' },
+   ]);
 
-    const handleInputChange = (event) => {
-        setInputValue(event.target.value);
-    };
+   // auto scroll to bottom
+   const messagesEndRef = useRef(null);
+   useEffect(() => {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+   }, [messageList]);
 
-    const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-            sendMessage();
-            event.preventDefault();
-        }
-    };
+   const handleKeyDown = (event) => {
+      if (event.key === 'Enter' && !event.shiftKey) {
+         event.preventDefault();
+         sendMessage();
+      }
+   };
 
-    const openai = new OpenAI({
-        apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-        dangerouslyAllowBrowser: true,
-    });
+   const sendMessage = async () => {
+      if (textAreaValue.trim() === '') {
+         return;
+      }
+      // add user message to chat
+      setMessageList((prevMessages) => [
+         ...prevMessages,
+         { text: textAreaValue, sender: 'user' },
+      ]);
+      // clear text area
+      setTextAreaValue('');
 
-    const sendMessage = async () => {
-        if (inputValue.trim() === '') {
-            return;
-        }
-        // add user message to chat
-        setMessageList((prevMessages) => [
-            ...prevMessages,
-            { text: inputValue, sender: 'user' },
-        ]);
-        // clear text area
-        setInputValue('');
+      try {
+         // create message
+         await openai.beta.threads.messages.create(thread.id, {
+            role: 'user',
+            content: textAreaValue,
+         });
+         // send message to openai assistant
+         const run = await openai.beta.threads.runs.create(thread.id, {
+            assistant_id: process.env.REACT_APP_OPENAI_ASSISTANT_ID,
+         });
 
-        // send message to OpenAI
-        try {
-            const thread = await openai.beta.threads.create();
-            await openai.beta.threads.messages.create(thread.id, {
-                role: 'user',
-                content: inputValue,
-            });
+         // wait for assistant response
+         let runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+         while (runStatus.status !== 'completed') {
+            // check once every second to see if assistant finished messge
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+         }
 
-            const run = await openai.beta.threads.runs.create(thread.id, {
-                assistant_id: process.env.REACT_APP_OPENAI_ASSISTANT_ID,
-            });
+         // get assistant response
+         const assistantResponse = (
+            await openai.beta.threads.messages.list(thread.id)
+         ).data.find((message) => message.role === 'assistant');
 
-            let runStatus = await openai.beta.threads.runs.retrieve(
-                thread.id,
-                run.id
-            );
+         const assistantMessage = assistantResponse.content[0].text.value;
 
-            while (runStatus.status !== 'completed') {
-                await new Promise((resolve) => setTimeout(resolve, 2000));
-                runStatus = await openai.beta.threads.runs.retrieve(
-                    thread.id,
-                    run.id
-                );
-            }
+         addMessageToChat(assistantMessage, 'assistant');
+      } catch (error) {
+         console.error(error);
+         console.error('Failed to send message to OpenAI');
+      }
+   };
 
-            const assistantMessage = await openai.beta.threads.messages.list(
-                thread.id
-            );
+   const addMessageToChat = (message, role) => {
+      setMessageList((prevMessages) => [
+         ...prevMessages,
+         { text: message, sender: role },
+      ]);
+   };
 
-            const lastMessageForRun = assistantMessage.data
-                .filter(
-                    (message) =>
-                        message.run_id === run.id &&
-                        message.role === 'assistant'
-                )
-                .pop();
-
-            if (lastMessageForRun) {
-                const assistantResponse =
-                    lastMessageForRun.content[0].text.value.replaceAll(
-                        /【.*?】/g,
-                        ''
-                    );
-
-                setMessageList((prevMessages) => [
-                    ...prevMessages,
-                    { text: assistantResponse, sender: 'shane' },
-                ]);
-            }
-        } catch (error) {
-            console.error(error);
-            console.error('Failed to send message to OpenAI');
-        }
-
-    };
-
-    useEffect(() => {
-        messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
-    }, [messageList]);
-
-    return (
-        <ScreenContainer>
-            {/* header */}
-            <ScreenHeader>
-                <HeaderBackBtn>🡠</HeaderBackBtn>
-                <HeaderProfile>
-                    <HeaderInfo></HeaderInfo>
-                    <ProfileImage src='face.png' alt='pfp' />
-                    <ProfileName>Shane</ProfileName>
-                </HeaderProfile>
-                <HeaderCallBtn>🕽</HeaderCallBtn>
-            </ScreenHeader>
-            {/* messages */}
-            <Messages>
-                {messageList.map((message, index) =>
-                    message.sender === 'user' ? (
-                        <SentMessage key={index}>{message.text}</SentMessage>
-                    ) : (
-                        <ReceivedMessage key={index}>
-                            {message.text}
-                        </ReceivedMessage>
-                    )
-                )}
-                <div ref={messagesEndRef} />
-            </Messages>
-            {/* input */}
-            <InputContainer>
-                <InputTextArea
-                    value={inputValue}
-                    placeholder='Send a message'
-                    onChange={handleInputChange}
-                    onKeyPress={handleKeyPress}
-                />
-            </InputContainer>
-        </ScreenContainer>
-    );
+   return (
+      <ScreenContainer>
+         {/* header */}
+         <ScreenHeader>
+            <HeaderBackBtn>🡠</HeaderBackBtn>
+            <HeaderProfile>
+               <ProfileImage src='pfp.png' alt='pfp' />
+               <ProfileName>Shane's Assistant</ProfileName>
+            </HeaderProfile>
+            <HeaderCallBtn>🕽</HeaderCallBtn>
+         </ScreenHeader>
+         {/* messages */}
+         <Messages>
+            {messageList.map((message, index) =>
+               message.sender === 'user' ? (
+                  <SentMessage key={index}>{message.text}</SentMessage>
+               ) : (
+                  <ReceivedMessage key={index}>{message.text}</ReceivedMessage>
+               )
+            )}
+            <div ref={messagesEndRef} />
+         </Messages>
+         {/* input */}
+         <InputContainer>
+            <InputTextArea
+               value={textAreaValue}
+               placeholder='Ask me about Shane!'
+               onChange={(e) => setTextAreaValue(e.target.value)}
+               onKeyDown={handleKeyDown}
+            />
+            <ChatButton onClick={sendMessage}>↑</ChatButton>
+         </InputContainer>
+      </ScreenContainer>
+   );
 };
 
 export default Screen;
--- src/components/phone/ScreenStyles.jsx
+++ src/components/phone/ScreenStyles.jsx
@@ -1,157 +1,167 @@
 import styled from 'styled-components';
 
 export const ScreenContainer = styled.div`
-    position: absolute;
-    width: 95%;
-    height: 97.5%;
-    border-radius: 40px;
-    background-color: #d5d5d5;
-    z-index: 1;
+   position: absolute;
+   width: 95%;
+   height: 97.5%;
+   border-radius: 40px;
+   background-color: #d5d5d5;
+   z-index: 1;
 
-    display: flex;
-    flex-direction: column;
-    /* justify-content: center; */
-    align-items: center;
+   display: flex;
+   flex-direction: column;
+   /* justify-content: center; */
+   align-items: center;
 `;
 
 // header
 export const ScreenHeader = styled.div`
-    width: 380px;
-    height: 135px;
-    border-radius: 40px 40px 0px 0px;
+   width: 380px;
+   height: 145px;
+   border-radius: 40px 40px 0px 0px;
 
-    background-color: #d5d5d597;
-    -webkit-backdrop-filter: blur(5px);
-    backdrop-filter: blur(25px);
+   background-color: #d5d5d597;
+   -webkit-backdrop-filter: blur(5px);
+   backdrop-filter: blur(25px);
 
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: end;
+   display: flex;
+   flex-direction: row;
+   justify-content: center;
+   align-items: end;
 
-    padding-bottom: 15px;
+   padding-bottom: 5px;
 
-    position: fixed;
-    z-index: 2;
+   position: fixed;
+   z-index: 2;
 `;
 
-export const HeaderInfo = styled.div`
-width:100%;
-height:20%;
-
-background-color:red;
-`
 
 export const HeaderProfile = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+   align-items: center;
+   height:62%;
+   
 `;
 
 export const ProfileImage = styled.img`
-    background-color: #a2a2a2;
-    border-radius: 50%;
-    width: 60px;
-    height: 60px;
-
-    object-fit: cover;
+   background-color: #a2a2a2;
+   border: solid 2px darkblue;
+   border-radius: 50%;
+   width: 60px;
+   height: 60px;
+   object-fit: cover;
+   
+   transition: all 0.2s ease-in-out;
+   &:active {
+      scale: 1.1;
+   }
 `;
 
 export const ProfileName = styled.span`
-    color: black;
-    text-align: center;
+   color: black;
+   text-align: center;
 `;
 
 export const HeaderBackBtn = styled.button`
-    font-size: 2rem;
-    margin-right: auto;
-    margin-left: 40px;
-    width: 40px;
-    height: 40px;
-    padding-bottom: 70px;
-    color:blue;
-    background: transparent;
-    border: none;
-    cursor:pointer;
-    transition: all 0.2s ease-in-out;
-    line-height: 50px;     
-    &:active {
-        font-size: 2.5rem;
-    }
+   font-size: 2rem;
+   margin-right: auto;
+   margin-left: 40px;
+   width: 40px;
+   height: 40px;
+   padding-bottom: 70px;
+   color: blue;
+   background: transparent;
+   border: none;
+   cursor: pointer;
 `;
 
 export const HeaderCallBtn = styled.button`
-    font-size: 2rem;
-    margin-left: auto;
-    margin-right: 40px;
-    width: 40px;
-    height: 40px;
-    padding-bottom: 70px;
-    color:blue;
-    background: transparent;
-    border: none;
-    cursor:pointer;
-    transition: all 0.2s ease-in-out;
-    line-height: 50px;     
-    &:active {
-        font-size: 2.5rem;
-    }
+   font-size: 2rem;
+   margin-left: auto;
+   margin-right: 40px;
+   width: 40px;
+   height: 40px;
+   padding-bottom: 70px;
+   color: blue;
+   background: transparent;
+   border: none;
+   cursor: pointer;
 `;
 
 // messages
 export const Messages = styled.div`
-    height: 100%;
-    width: 100%;
-    padding-top:150px;
-    border-radius: 40px 40px 0px 0px;
-    background-color: #ffffff;
-    overflow-y: auto;
-    transition: all 2s ease;
-    scrollbar-width: none;
-    scroll-behavior: smooth;
+   height: 100%;
+   width: 100%;
+   padding-top: 150px;
+   border-radius: 40px 40px 0px 0px;
+   background-color: #ffffff;
+   overflow-y: auto;
+   transition: all 2s ease;
+   scrollbar-width: none;
+   scroll-behavior: smooth;
 `;
 
 export const SentMessage = styled.div`
-    background-color: #007bff;
-    margin: 5px 25px 5px auto;
-    color: #fff;
-    border-radius: 10px 10px 0px 10px;
-    padding: 8px;
-    max-width: 70%;
-    width: fit-content;
+   background-color: #007bff;
+   margin: 5px 25px 5px auto;
+   color: #fff;
+   border-radius: 10px 10px 0px 10px;
+   padding: 8px;
+   max-width: 70%;
+   width: fit-content;
 `;
 
 export const ReceivedMessage = styled.div`
-    background-color: #e0e0e0;
-    margin: 5px auto 5px 25px;
-    color: #000;
-    border-radius: 10px 10px 10px 0px;
-    padding: 8px;
-    max-width: 70%;
-    width: fit-content;
+   background-color: #e0e0e0;
+   margin: 5px auto 5px 25px;
+   color: #000;
+   border-radius: 10px 10px 10px 0px;
+   padding: 8px;
+   max-width: 70%;
+   width: fit-content;
 `;
 
 // input
 export const InputContainer = styled.div`
-    height: 150px;
-    width: 380px;
-    padding-top:5px;
-    border-radius: 0px 0px 40px 40px;
-    background-color: #d5d5d5;
+   height: 150px;
+   width: 380px;
+   padding-top: 5px;
+   border-radius: 0px 0px 40px 40px;
+   background-color: #d5d5d5;
 
-    display: flex;
-    justify-content: center;
+   display: flex;
+   justify-content: center;
 `;
 
 export const InputTextArea = styled.textarea`
-    width: 70%;
-    height: 60px;
-    border: 1px solid #ccc;
-    border-radius: 25px;
-    font-size: 1rem;
-    resize: none;
-    outline: none;
-    padding-left:15px;
-    padding-right:15px;
+   width: 60%;
+   height: 60px;
+   border: 1px solid #ccc;
+   border-radius: 15px;
+   font-size: 1rem;
+   resize: none;
+   outline: none;
+   padding-left: 15px;
+   padding-right: 15px;
+`;
+
+export const ChatButton = styled.button`
+   background-color: transparent;
+   min-height: 55px;
+   min-width: 55px;
+   max-height: 55px;
+   max-width: 55px;
+   color: #fff;
+   border: none;
+   cursor: pointer;
+   font-size: 3rem;
+   transition: scale 0.2s ease-in-out;
+   line-height: 50px;
+   color:darkblue;
+
+   &:active {
+      scale:1.25;
+   }
 `;
